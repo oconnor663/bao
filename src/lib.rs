2cf9c4ba664ec978fd89ab1c98f4e9591f92a300
Jack O'Connor <oconnor663@gmail.com>
2016-12-05 02:58:34 -0500

    an untested Read implementation

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,9 @@ extern crate ring;
 
 use ring::{constant_time, digest};
 use ring::error::Unspecified;
+use std::cmp::min;
+use std::io;
+use std::io::prelude::*;
 
 pub const CHUNK_SIZE: usize = 4096;
 pub const DIGEST_SIZE: usize = 32;
@@ -23,7 +26,7 @@ fn verify(input: &[u8], digest: &Digest) -> Result<(), Unspecified> {
     constant_time::verify_slices_are_equal(&digest[..], &computed[..])
 }
 
-fn left_input_len(input_len: usize) -> usize {
+fn left_plaintext_len(input_len: usize) -> usize {
     // Find the first power of 2 times the chunk size that is *strictly* less than the input
     // length. So if the input is exactly 4 chunks long, for example, the answer here will be 2
     // chunks.
@@ -39,7 +42,7 @@ pub fn encode(input: &[u8]) -> (Vec<u8>, Digest) {
     if input.len() <= CHUNK_SIZE {
         return (input.to_vec(), hash(input));
     }
-    let left_len = left_input_len(input.len());
+    let left_len = left_plaintext_len(input.len());
     let (left_encoded, left_hash) = encode(&input[..left_len]);
     let (right_encoded, right_hash) = encode(&input[left_len..]);
     let mut node = [0; 2 * DIGEST_SIZE];
@@ -112,11 +115,97 @@ pub fn decode_chunk<'a>(encoded: &'a [u8],
     }
 }
 
+pub struct RahReader<T> {
+    inner_reader: T,
+    traversal_stack: Vec<(Digest, usize)>,
+    read_buffer: Vec<u8>,
+    output_buffer: Vec<u8>,
+}
+
+impl<T> RahReader<T> {
+    pub fn new(inner_reader: T, digest: &Digest, plaintext_len: usize) -> Self {
+        RahReader {
+            inner_reader: inner_reader,
+            traversal_stack: vec![(*digest, plaintext_len)],
+            read_buffer: Vec::new(),
+            output_buffer: Vec::new(),
+        }
+    }
+}
+
+impl<T: Read> Read for RahReader<T> {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        // If there's no data in the chunk buffer, and we haven't reached the end of the tree yet,
+        // try to traverse the tree down to a chunk and read it.
+        if self.output_buffer.len() == 0 && self.traversal_stack.len() > 0 {
+            // Keep traversing nodes until we get down to the size of a single chunk.
+            loop {
+                let (digest, plaintext_len) = *self.traversal_stack.last().unwrap();
+                if plaintext_len <= CHUNK_SIZE {
+                    break;
+                }
+                fill_vec(&mut self.inner_reader,
+                         &mut self.read_buffer,
+                         2 * DIGEST_SIZE)?;
+                as_io_error(verify(&self.read_buffer[..], &digest))?;
+                let left_len = left_plaintext_len(plaintext_len);
+                let left_digest = *array_ref![self.read_buffer, 0, DIGEST_SIZE];
+                let right_len = plaintext_len - left_len;
+                let right_digest = *array_ref![self.read_buffer, DIGEST_SIZE, DIGEST_SIZE];
+                self.read_buffer.clear();
+                self.traversal_stack.pop();
+                self.traversal_stack.push((right_digest, right_len));
+                self.traversal_stack.push((left_digest, left_len));
+            }
+            // Then read that chunk.
+            let (digest, plaintext_len) = *self.traversal_stack.last().unwrap();
+            debug_assert!(plaintext_len <= CHUNK_SIZE);
+            fill_vec(&mut self.inner_reader, &mut self.read_buffer, plaintext_len)?;
+            as_io_error(verify(&self.read_buffer[..], &digest))?;
+            self.traversal_stack.pop();
+            debug_assert!(self.output_buffer.len() == 0);
+            std::mem::swap(&mut self.read_buffer, &mut self.output_buffer);
+        }
+
+        // Finally, return as much as we can from the chunk buffer. That might be zero bytes, which
+        // would mean EOF.
+        let write_len = min(self.output_buffer.len(), buf.len());
+        (&mut buf[0..write_len]).copy_from_slice(&mut self.output_buffer[0..write_len]);
+        self.output_buffer.drain(0..write_len);
+        Ok(write_len)
+    }
+}
+
+impl<T: Seek + Read> Seek for RahReader<T> {
+    fn seek(&mut self, pos: io::SeekFrom) -> io::Result<u64> {
+        unimplemented!();
+    }
+}
+
+fn fill_vec<R: Read>(reader: &mut R, vec: &mut Vec<u8>, target_len: usize) -> io::Result<usize> {
+    let mut read_buf = [0u8; 4096];
+    let mut total_read = 0;
+    while vec.len() < target_len {
+        let bytes_wanted = min(target_len - vec.len(), read_buf.len());
+        let bytes_got = reader.read(&mut read_buf[0..bytes_wanted])?;
+        if bytes_got == 0 {
+            return Err(io::Error::new(io::ErrorKind::UnexpectedEof, "EOF during fill_vec"));
+        }
+        total_read += bytes_got;
+        vec.extend_from_slice(&read_buf[0..bytes_got]);
+    }
+    Ok(total_read)
+}
+
+fn as_io_error<T>(result: Result<T, Unspecified>) -> io::Result<T> {
+    result.or(Err(io::Error::new(io::ErrorKind::InvalidData, "hash mismatch")))
+}
+
 #[cfg(test)]
 mod test {
     use std::cmp::min;
     use super::*;
-    use super::{hash, verify, left_input_len, left_subtree_len_and_chunk_count};
+    use super::{hash, verify, left_plaintext_len, left_subtree_len_and_chunk_count};
 
     fn debug_sample(input: &[u8]) -> String {
         let sample_len = min(60, input.len());
@@ -136,14 +225,14 @@ mod test {
     }
 
     #[test]
-    fn test_left_input_len() {
+    fn test_left_plaintext_len() {
         let cases = &[(CHUNK_SIZE + 1, CHUNK_SIZE),
                       (2 * CHUNK_SIZE - 1, CHUNK_SIZE),
                       (2 * CHUNK_SIZE, CHUNK_SIZE),
                       (2 * CHUNK_SIZE + 2, 2 * CHUNK_SIZE)];
         for &case in cases {
             println!("testing {} and {}", case.0, case.1);
-            assert_eq!(left_input_len(case.0), case.1);
+            assert_eq!(left_plaintext_len(case.0), case.1);
         }
     }
 
