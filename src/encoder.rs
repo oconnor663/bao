147ac9ee1dbe2b150d94f2d156abca5f9ea8df7b
Jack O'Connor <oconnor663@gmail.com>
2017-09-30 19:34:01 -0400

    test the PostOrderEncoder

--- src/encoder.rs
+++ src/encoder.rs
@@ -3,26 +3,61 @@ use byteorder::{ByteOrder, BigEndian};
 // TODO: Factor this type up and combine it with Region.
 #[derive(Debug, Copy, Clone)]
 struct Subtree {
-    hash: ::Digest,
     len: u64,
+    hash: ::Digest,
 }
 
 impl Subtree {
+    #[cfg(test)]
+    fn encoded_len(&self) -> u64 {
+        // Divide rounding up.
+        let num_chunks = (self.len / ::CHUNK_SIZE as u64) +
+            (self.len % ::CHUNK_SIZE as u64 > 0) as u64;
+        // Note that the empty input results in zero nodes, not "-1" nodes.
+        self.len + num_chunks.saturating_sub(1) * ::NODE_SIZE as u64
+    }
+
     fn join(&self, rhs: &Subtree) -> Subtree {
-        let mut node = [0; 2 * ::CHUNK_SIZE];
-        node[..::CHUNK_SIZE].copy_from_slice(&self.hash);
-        node[::CHUNK_SIZE..].copy_from_slice(&rhs.hash);
+        let mut node = [0; 2 * ::DIGEST_SIZE];
+        node[..::DIGEST_SIZE].copy_from_slice(&self.hash);
+        node[::DIGEST_SIZE..].copy_from_slice(&rhs.hash);
         Subtree {
             len: self.len.checked_add(rhs.len).expect("len overflow"),
             hash: ::hash(&node),
         }
     }
 
-    fn to_header(&self) -> [u8; ::HEADER_SIZE] {
+    #[cfg(test)]
+    fn children(&self, unverified_node: &::evil::EvilBytes) -> ::Result<(Subtree, Subtree)> {
+        let verified_bytes = unverified_node.verify(::NODE_SIZE, &self.hash)?;
+        let left = Subtree {
+            len: ::left_len(self.len),
+            hash: *array_ref!(verified_bytes, 0, ::DIGEST_SIZE),
+        };
+        let right = Subtree {
+            len: self.len - left.len,
+            hash: *array_ref!(verified_bytes, ::DIGEST_SIZE, ::DIGEST_SIZE),
+        };
+        Ok((left, right))
+    }
+
+    fn to_header_bytes(&self) -> [u8; ::HEADER_SIZE] {
         let mut ret = [0; ::HEADER_SIZE];
         BigEndian::write_u64(&mut ret[..8], self.len);
         ret[8..].copy_from_slice(&self.hash);
         ret
+    }
+
+    #[cfg(test)]
+    fn from_header_bytes(
+        unverified_header: &::evil::EvilBytes,
+        hash: &::Digest,
+    ) -> ::Result<Subtree> {
+        let verified_bytes = unverified_header.verify(::HEADER_SIZE, hash)?;
+        Ok(Subtree {
+            len: BigEndian::read_u64(&verified_bytes[..8]),
+            hash: *array_ref!(verified_bytes, 8, ::DIGEST_SIZE),
+        })
     }
 
     fn from_chunk(chunk: &[u8]) -> Subtree {
@@ -52,6 +87,14 @@ pub struct PostOrderEncoder {
 }
 
 impl PostOrderEncoder {
+    pub fn new() -> Self {
+        Self {
+            stack: Vec::new(),
+            out_buf: Vec::new(),
+            finalized: false,
+        }
+    }
+
     pub fn feed(&mut self, input: &[u8; ::CHUNK_SIZE]) -> &[u8] {
         if self.finalized {
             panic!("feed called on a finalized encoder");
@@ -64,6 +107,10 @@ impl PostOrderEncoder {
         while self.stack.len() >= 2 {
             let left = self.stack[self.stack.len() - 2];
             let right = self.stack[self.stack.len() - 1];
+            // In the feed loop, we only build full trees, where the left and
+            // right are the same length. All the partial trees (with their
+            // roots on the right edge of the final edge) get constructed
+            // during finalize() below.
             if left.len != right.len {
                 break;
             }
@@ -76,10 +123,14 @@ impl PostOrderEncoder {
         &self.out_buf
     }
 
-    pub fn finalize(&mut self, input: &[u8]) -> &[u8] {
+    pub fn finalize(&mut self, input: &[u8]) -> (&[u8], ::Digest) {
         if self.finalized {
             panic!("finalize called on a finalized encoder");
         }
+        assert!(
+            input.len() < ::CHUNK_SIZE,
+            "full chunk or more passed to finalize"
+        );
         self.finalized = true;
         self.out_buf.clear();
         if input.len() > 0 {
@@ -103,7 +154,76 @@ impl PostOrderEncoder {
         // Take the the final remaining subtree, or the empty subtree if there
         // was never any input, and turn it into the header.
         let root = self.stack.pop().unwrap_or_else(Subtree::empty);
-        self.out_buf.extend_from_slice(&root.to_header());
-        &self.out_buf
+        let header_bytes = root.to_header_bytes();
+        self.out_buf.extend_from_slice(&header_bytes);
+        (&self.out_buf, ::hash(&header_bytes))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    // Very similar to the simple decoder function, but for a post-order tree.
+    fn validate_post_order_encoding(encoded: &::evil::EvilBytes, hash: &::Digest) {
+        let header_start = encoded.len() - ::HEADER_SIZE;
+        let header_encoded = encoded.slice(header_start, encoded.len());
+        let header = Subtree::from_header_bytes(&header_encoded, hash).expect("bad header");
+        let encoded_rest = encoded.slice(0, header_start);
+        assert_eq!(
+            header.encoded_len(),
+            encoded_rest.len() as u64,
+            "encoded len doesn't make sense"
+        );
+        validate_post_order_encoding_inner(&encoded_rest, &header);
+    }
+
+    fn validate_post_order_encoding_inner(encoded: &::evil::EvilBytes, subtree: &Subtree) {
+        if encoded.len() <= ::CHUNK_SIZE {
+            encoded.verify(encoded.len(), &subtree.hash).expect(
+                "bad chunk",
+            );
+            return;
+        }
+        let node_start = encoded.len() - ::NODE_SIZE;
+        let node_encoded = encoded.slice(node_start, encoded.len());
+        let (left_subtree, right_subtree) = subtree.children(&node_encoded).expect("bad node");
+        let left_encoded = encoded.slice(0, left_subtree.encoded_len() as usize);
+        validate_post_order_encoding_inner(&left_encoded, &left_subtree);
+        let right_encoded = encoded.slice(left_encoded.len(), node_start);
+        validate_post_order_encoding_inner(&right_encoded, &right_subtree);
+    }
+
+    fn post_order_encode_all(mut input: &[u8]) -> (Vec<u8>, ::Digest) {
+        let mut encoder = PostOrderEncoder::new();
+        let mut output = Vec::new();
+        while input.len() >= ::CHUNK_SIZE {
+            let out_slice = encoder.feed(array_ref!(input, 0, ::CHUNK_SIZE));
+            output.extend_from_slice(out_slice);
+            input = &input[::CHUNK_SIZE..];
+        }
+        let (out_slice, hash) = encoder.finalize(input);
+        output.extend_from_slice(out_slice);
+        (output, hash)
+    }
+
+    #[test]
+    fn test_post_order_encoder() {
+        for &case in ::TEST_CASES {
+            println!("starting case {}", case);
+            let input = vec![0x33; case];
+            let (encoded, hash) = post_order_encode_all(&input);
+            validate_post_order_encoding(&::evil::EvilBytes::wrap(&encoded), &hash);
+
+            // Also compare against the hash from the standard encoding.
+            // Despite the difference in tree layout, the hashes should all be
+            // the same.
+            let (_, regular_hash) = ::simple::encode(&input);
+            assert_eq!(
+                regular_hash,
+                hash,
+                "post order hash doesn't match the standard encoding"
+            );
+        }
     }
 }
--- src/evil.rs
+++ src/evil.rs
@@ -1,3 +1,4 @@
+// TODO: make this Copy?
 /// A tiny wrapper around bytes that guarantees we check their hash before we
 /// use them.
 pub struct EvilBytes<'a>(&'a [u8]);
@@ -7,6 +8,7 @@ impl<'a> EvilBytes<'a> {
         EvilBytes(bytes)
     }
 
+    // TODO: Get rid of the len param?
     /// Take a slice from the evil input, but only if its length and hash match
     /// what we expect.
     pub fn verify(&self, len: usize, hash: &::Digest) -> ::Result<&'a [u8]> {
@@ -17,11 +19,24 @@ impl<'a> EvilBytes<'a> {
         Ok(&self.0[..len])
     }
 
+    // TODO: Get rid of this function, in favor of computing encoded_len.
     /// Same as `verify`, but also bump the start of the input forward on
     /// success.
     pub fn verify_bump(&mut self, len: usize, hash: &::Digest) -> ::Result<&'a [u8]> {
         let output = self.verify(len, hash)?;
         self.0 = &self.0[len..];
         Ok(output)
+    }
+
+    /// Get the length of the underlying slice.
+    #[cfg(test)]
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    /// Take a subslice of the unverified input.
+    #[cfg(test)]
+    pub fn slice(&self, start: usize, end: usize) -> EvilBytes<'a> {
+        EvilBytes(&self.0[start..end])
     }
 }
