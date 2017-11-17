fcb0bab0589cedb84c1111f985fdcafcf759a1c2
Jack O'Connor <oconnor663@gmail.com>
2017-11-16 22:58:12 -0500

    change bao.py to use a length-only header

--- python/bao.py
+++ python/bao.py
@@ -15,49 +15,48 @@ CHUNK_SIZE = 4096
 DIGEST_SIZE = 32
 HEADER_SIZE = 8 + DIGEST_SIZE
 
-def blake2b_256(b):
-    return hashlib.blake2b(b, digest_size=32).digest()
+def blake2b_256(buf):
+    return hashlib.blake2b(buf, digest_size=32).digest()
 
 def left_len(total_len):
     available_chunks = (total_len - 1) // CHUNK_SIZE
     power_of_two_chunks = 2 ** (available_chunks.bit_length() - 1)
     return CHUNK_SIZE * power_of_two_chunks
 
-def bao_encode(b):
-    def encode_tree(b):
-        if len(b) <= CHUNK_SIZE:
-            return blake2b_256(b), b
-        left_len_ = left_len(len(b))
-        left_hash, left_encoded = encode_tree(b[:left_len_])
-        right_hash, right_encoded = encode_tree(b[left_len_:])
-        node = left_hash + right_hash
+def bao_encode(buf):
+    def encode_recurse(buf, prefix):
+        if len(buf) <= CHUNK_SIZE:
+            return blake2b_256(prefix + buf), prefix + buf
+        llen = left_len(len(buf))
+        left_hash, left_encoded = encode_recurse(buf[:llen], b"")
+        right_hash, right_encoded = encode_recurse(buf[llen:], b"")
+        node = prefix + left_hash + right_hash
         return blake2b_256(node), node + left_encoded + right_encoded
 
-    root_hash, encoded = encode_tree(b)
-    header = len(b).to_bytes(8, "little") + root_hash
-    return blake2b_256(header), header + encoded
+    prefix = len(buf).to_bytes(8, "little")
+    return encode_recurse(buf, prefix)
 
-def bao_decode(header_hash, b):
-    def decode_tree(tree_hash, content_len, offset, b):
+def bao_decode(buf, digest):
+    def decode_recurse(buf, digest, encoded_offset, content_len, prefix):
         if content_len <= CHUNK_SIZE:
-            new_offset = offset + content_len
-            chunk = b[offset:new_offset]
-            assert tree_hash == blake2b_256(chunk)
+            new_offset = encoded_offset + content_len
+            chunk = buf[encoded_offset:new_offset]
+            if digest is not None:
+                assert digest == blake2b_256(prefix + chunk)
             return new_offset, chunk
-        new_offset = offset + 2*DIGEST_SIZE
-        node = b[offset:new_offset]
-        assert tree_hash == blake2b_256(node)
+        new_offset = encoded_offset + 2*DIGEST_SIZE
+        node = buf[encoded_offset:new_offset]
+        if digest is not None:
+            assert digest == blake2b_256(prefix + node)
         left_hash, right_hash = node[:DIGEST_SIZE], node[DIGEST_SIZE:]
-        left_len_ = left_len(content_len)
-        new_offset, left_decoded = decode_tree(left_hash, left_len_, new_offset, b)
-        new_offset, right_decoded = decode_tree(right_hash, content_len - left_len_, new_offset, b)
+        llen = left_len(content_len)
+        new_offset, left_decoded = decode_recurse(buf, left_hash, new_offset, llen, b"")
+        new_offset, right_decoded = decode_recurse(buf, right_hash, new_offset, content_len - llen, b"")
         return new_offset, left_decoded + right_decoded
 
-    header = b[:HEADER_SIZE]
-    assert header_hash == blake2b_256(header)
-    content_len = int.from_bytes(header[:8], "little")
-    tree_hash = header[8:]
-    _, decoded = decode_tree(tree_hash, content_len, HEADER_SIZE, b)
+    assert len(buf) >= 8, "not enough bytes for header"
+    content_len = int.from_bytes(buf[:8], "little")
+    _, decoded = decode_recurse(buf, digest, 8, content_len, buf[:8])
     return decoded
 
 def main():
@@ -66,12 +65,12 @@ def main():
         _, encoded = bao_encode(sys.stdin.buffer.read())
         sys.stdout.buffer.write(encoded)
     elif args["decode"]:
-        input_ = sys.stdin.buffer.read()
+        buf = sys.stdin.buffer.read()
         if args["--any"]:
-            header_hash = blake2b_256(input_[:HEADER_SIZE])
+            header_hash = None
         else:
             header_hash = binascii.unhexlify(args["--hash"])
-        decoded = bao_decode(header_hash, input_)
+        decoded = bao_decode(buf, header_hash)
         sys.stdout.buffer.write(decoded)
     elif args["hash"]:
         bao_hash, _ = bao_encode(sys.stdin.buffer.read())
