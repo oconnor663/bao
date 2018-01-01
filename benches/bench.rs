28c32cf0a576388f485a16573c4e78d11820b609
Jack O'Connor <oconnor663@gmail.com>
2018-01-01 13:09:03 -0500

    benchmark the simple encode and decode functions

--- benches/bench.rs
+++ benches/bench.rs
@@ -28,3 +28,25 @@ fn bench_blake2b_long(b: &mut Bencher) {
 fn bench_bao_hash_long(b: &mut Bencher) {
     b.iter(|| { bao::hash::hash(LONG); });
 }
+
+#[bench]
+fn bench_bao_encode_simple_short(b: &mut Bencher) {
+    b.iter(|| { bao::simple::encode(SHORT); });
+}
+
+#[bench]
+fn bench_bao_encode_simple_long(b: &mut Bencher) {
+    b.iter(|| { bao::simple::encode(LONG); });
+}
+
+#[bench]
+fn bench_bao_decode_simple_short(b: &mut Bencher) {
+    let (encoded, hash) = bao::simple::encode(SHORT);
+    b.iter(|| { bao::simple::decode(&encoded, &hash).unwrap(); });
+}
+
+#[bench]
+fn bench_bao_decode_simple_long(b: &mut Bencher) {
+    let (encoded, hash) = bao::simple::encode(LONG);
+    b.iter(|| { bao::simple::decode(&encoded, &hash).unwrap(); });
+}
