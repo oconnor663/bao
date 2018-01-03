d34774a4780b55d7636352d0d39ff66bff5da65e
Jack O'Connor <oconnor663@gmail.com>
2018-01-03 18:08:03 -0500

    make hash_parallel3.rs do less copying

--- src/hash_parallel3.rs
+++ src/hash_parallel3.rs
@@ -24,25 +24,39 @@ impl State {
         }
     }
 
-    pub fn update(&mut self, mut input: &[u8]) {
-        while !input.is_empty() {
-            if self.buf.len() == BUFSIZE {
-                let hash = ::hash_parallel2::hash_recurse(&self.buf, &[]);
-                self.nodes.push(hash);
-                self.buf.clear();
-                // Same trick from hash.rs.
-                while self.nodes.len() > (self.count / BUFSIZE as u64).count_ones() as usize {
-                    let right = self.nodes.pop().unwrap();
-                    let left = self.nodes.pop().unwrap();
-                    self.nodes.push(::hash_two(&left, &right));
-                }
-            }
-            let want = BUFSIZE - self.buf.len();
-            let take = cmp::min(want, input.len());
-            self.buf.extend_from_slice(&input[..take]);
-            input = &input[take..];
-            self.count += take as u64;
+    fn push_node(&mut self, new_node: ::Digest) {
+        self.nodes.push(new_node);
+        // Same trick from hash.rs.
+        while self.nodes.len() > (self.count / BUFSIZE as u64).count_ones() as usize {
+            let right = self.nodes.pop().unwrap();
+            let left = self.nodes.pop().unwrap();
+            self.nodes.push(::hash_two(&left, &right));
         }
+    }
+
+    pub fn update(&mut self, mut input: &[u8]) {
+        // If there's already input in our buffer, we need to try to fill it.
+        if self.buf.len() > 0 {
+            self.count += fill_buf(&mut self.buf, &mut input, BUFSIZE) as u64;
+            if !input.is_empty() {
+                let hash = ::hash_parallel2::hash_recurse(&self.buf, &[]);
+                self.buf.clear();
+                self.push_node(hash);
+            } else {
+                return;
+            }
+        }
+        // Now as long as there are BUFSIZE chunks in the input (plus at least
+        // one more byte to be sure the chunk doesn't need finalization), hash
+        // them directly, to avoid copying overhead.
+        while input.len() > BUFSIZE {
+            self.count += BUFSIZE as u64;
+            let hash = ::hash_parallel2::hash_recurse(&input[..BUFSIZE], &[]);
+            input = &input[BUFSIZE..];
+            self.push_node(hash);
+        }
+        // Finally, add any remaining input to the buffer.
+        self.count += fill_buf(&mut self.buf, &mut input, BUFSIZE) as u64;
     }
 
     pub fn finalize(&mut self) -> ::Digest {
@@ -63,6 +77,14 @@ impl State {
             self.nodes.push(::finalize_node(&mut parent_digest));
         }
     }
+}
+
+fn fill_buf(buf: &mut Vec<u8>, input: &mut &[u8], target: usize) -> usize {
+    let want = target - buf.len();
+    let take = cmp::min(want, input.len());
+    buf.extend_from_slice(&input[..take]);
+    *input = &input[take..];
+    take
 }
 
 #[cfg(test)]
