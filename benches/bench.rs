dfe7bea7588dd87f6e384bbda086f36c1d9e8d11
Jack O'Connor <oconnor663@gmail.com>
2018-07-03 10:46:31 -0400

    make benchmarks auto-black-box the return value

--- benches/bench.rs
+++ benches/bench.rs
@@ -13,65 +13,47 @@ const LONG: &[u8] = &[0; 1_000_000];
 
 #[bench]
 fn bench_blake2b_short(b: &mut Bencher) {
-    b.iter(|| {
-        blake2_c::blake2b_256(SHORT);
-    });
+    b.iter(|| blake2_c::blake2b_256(SHORT));
 }
 
 #[bench]
 fn bench_blake2b_medium(b: &mut Bencher) {
-    b.iter(|| {
-        blake2_c::blake2b_256(MEDIUM);
-    });
+    b.iter(|| blake2_c::blake2b_256(MEDIUM));
 }
 
 #[bench]
 fn bench_blake2b_long(b: &mut Bencher) {
-    b.iter(|| {
-        blake2_c::blake2b_256(LONG);
-    });
+    b.iter(|| blake2_c::blake2b_256(LONG));
 }
 
 #[bench]
 fn bench_bao_hash_short(b: &mut Bencher) {
-    b.iter(|| {
-        bao::hash::hash(SHORT);
-    });
+    b.iter(|| bao::hash::hash(SHORT));
 }
 
 #[bench]
 fn bench_bao_hash_medium(b: &mut Bencher) {
-    b.iter(|| {
-        bao::hash::hash(MEDIUM);
-    });
+    b.iter(|| bao::hash::hash(MEDIUM));
 }
 
 #[bench]
 fn bench_bao_hash_long(b: &mut Bencher) {
-    b.iter(|| {
-        bao::hash::hash(LONG);
-    });
+    b.iter(|| bao::hash::hash(LONG));
 }
 
 #[bench]
 fn bench_bao_hash_parallel_short(b: &mut Bencher) {
-    b.iter(|| {
-        bao::hash::hash_parallel(SHORT);
-    });
+    b.iter(|| bao::hash::hash_parallel(SHORT));
 }
 
 #[bench]
 fn bench_bao_hash_parallel_medium(b: &mut Bencher) {
-    b.iter(|| {
-        bao::hash::hash_parallel(MEDIUM);
-    });
+    b.iter(|| bao::hash::hash_parallel(MEDIUM));
 }
 
 #[bench]
 fn bench_bao_hash_parallel_long(b: &mut Bencher) {
-    b.iter(|| {
-        bao::hash::hash_parallel(LONG);
-    });
+    b.iter(|| bao::hash::hash_parallel(LONG));
 }
 
 #[bench]
@@ -79,7 +61,7 @@ fn bench_bao_hash_writer_short(b: &mut Bencher) {
     b.iter(|| {
         let mut writer = bao::hash::Writer::new();
         writer.write_all(SHORT).unwrap();
-        writer.finish();
+        writer.finish()
     });
 }
 
@@ -88,7 +70,7 @@ fn bench_bao_hash_writer_medium(b: &mut Bencher) {
     b.iter(|| {
         let mut writer = bao::hash::Writer::new();
         writer.write_all(MEDIUM).unwrap();
-        writer.finish();
+        writer.finish()
     });
 }
 
@@ -97,6 +79,6 @@ fn bench_bao_hash_writer_long(b: &mut Bencher) {
     b.iter(|| {
         let mut writer = bao::hash::Writer::new();
         writer.write_all(LONG).unwrap();
-        writer.finish();
+        writer.finish()
     });
 }
