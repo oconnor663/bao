dc4171881d0d4e7d721eb7180c65f2ebb30c329d
Jack O'Connor <oconnor663@gmail.com>
2018-07-29 22:42:08 -0400

    run the Python tests as part of `cargo test`

--- src/lib.rs
+++ src/lib.rs
@@ -18,3 +18,25 @@ pub mod encode;
 pub mod hash;
 pub mod io;
 mod unverified;
+
+#[cfg(test)]
+mod test {
+    use std::process::Command;
+
+    #[test]
+    fn run_python_tests() {
+        let output = Command::new("python3")
+            .arg("./python/test.py")
+            .output()
+            .expect("Python test script failed to run.");
+        println!(
+            "=== stdout ===\n{}",
+            String::from_utf8_lossy(&output.stdout)
+        );
+        println!(
+            "=== stderr ===\n{}",
+            String::from_utf8_lossy(&output.stderr)
+        );
+        assert!(output.status.success(), "Python tests failed.");
+    }
+}
