c604ef72c5f954c4c27453475df71f02d352f734
Jack O'Connor <oconnor663@gmail.com>
2018-08-13 21:27:07 -0400

    move benchmarks to blake2b_simd

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,7 +1,7 @@
 #![feature(test)]
 
 extern crate bao;
-extern crate blake2_c;
+extern crate blake2b_simd;
 extern crate test;
 
 use std::io::prelude::*;
@@ -13,17 +13,17 @@ const LONG: &[u8] = &[0; 1_000_000];
 
 #[bench]
 fn bench_blake2b_short(b: &mut Bencher) {
-    b.iter(|| blake2_c::blake2b_256(SHORT));
+    b.iter(|| blake2b_simd::blake2b(SHORT));
 }
 
 #[bench]
 fn bench_blake2b_medium(b: &mut Bencher) {
-    b.iter(|| blake2_c::blake2b_256(MEDIUM));
+    b.iter(|| blake2b_simd::blake2b(MEDIUM));
 }
 
 #[bench]
 fn bench_blake2b_long(b: &mut Bencher) {
-    b.iter(|| blake2_c::blake2b_256(LONG));
+    b.iter(|| blake2b_simd::blake2b(LONG));
 }
 
 #[bench]
