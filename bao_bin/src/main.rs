fb1fbfd82fae5594b47c733ae68532688c5f7057
Jack O'Connor <oconnor663@gmail.com>
2018-09-05 00:31:27 -0400

    add the `bao slice` and `bao decode-slice` commands

--- bao_bin/src/main.rs
+++ bao_bin/src/main.rs
@@ -9,7 +9,7 @@ extern crate os_pipe;
 extern crate serde_derive;
 extern crate memmap;
 
-use failure::Error;
+use failure::{err_msg, Error};
 use std::fs::{File, OpenOptions};
 use std::io;
 use std::io::prelude::*;
@@ -21,6 +21,8 @@ const USAGE: &str = "
 Usage: bao hash [<input>] [--encoded]
        bao encode [<input>] [<output>]
        bao decode <hash> [<input>] [<output>] [--seek=<offset>]
+       bao slice <start> <len> [<input>] [<output>]
+       bao decode-slice <hash> <start> <len> [<input>] [<output>]
        bao (--help | --version)
 ";
 
@@ -29,9 +31,13 @@ struct Args {
     cmd_decode: bool,
     cmd_encode: bool,
     cmd_hash: bool,
+    cmd_slice: bool,
+    cmd_decode_slice: bool,
     arg_input: Option<PathBuf>,
     arg_output: Option<PathBuf>,
     arg_hash: String,
+    arg_start: u64,
+    arg_len: u64,
     flag_encoded: bool,
     flag_help: bool,
     flag_seek: Option<u64>,
@@ -60,6 +66,12 @@ fn main() -> Result<(), Error> {
         encode(&args, in_file, out_file)?;
     } else if args.cmd_decode {
         decode(&args, in_file, out_file)?;
+    } else if args.cmd_slice {
+        slice(&args, in_file, out_file)?;
+    } else if args.cmd_decode_slice {
+        decode_slice(&args, in_file, out_file)?;
+    } else {
+        unreachable!();
     }
 
     Ok(())
@@ -94,12 +106,7 @@ fn encode(_args: &Args, mut in_file: File, out_file: File) -> Result<(), Error> 
     }
     // If one or both of the files weren't mappable, fall back to the writer. First check that we
     // have an actual file and not a pipe, because the writer requires seek.
-    if !out_file.metadata()?.is_file() {
-        Err(io::Error::new(
-            io::ErrorKind::InvalidInput,
-            "encode output must be a real file",
-        ))?;
-    }
+    confirm_real_file(&out_file, "encode output")?;
     let mut writer = bao::encode::Writer::new(out_file);
     io::copy(&mut in_file, &mut writer)?;
     writer.finish()?;
@@ -107,30 +114,37 @@ fn encode(_args: &Args, mut in_file: File, out_file: File) -> Result<(), Error> 
 }
 
 fn decode(args: &Args, in_file: File, mut out_file: File) -> Result<(), Error> {
-    let hash_vec = hex::decode(&args.arg_hash)
-        .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid hex"))?;
-    if hash_vec.len() != bao::hash::HASH_SIZE {
-        Err(io::Error::new(
-            io::ErrorKind::InvalidInput,
-            "wrong length hash",
-        ))?;
-    };
-    let hash = array_ref!(hash_vec, 0, bao::hash::HASH_SIZE);
+    let hash = parse_hash(args)?;
     // If we're not seeking, try to memmap the files.
     if args.flag_seek.is_none() {
         if let Some(in_map) = maybe_memmap_input(&in_file)? {
             let content_len = bao::decode::parse_and_check_content_len(&in_map)?;
             if let Some(mut out_map) = maybe_memmap_output(&out_file, content_len as u128)? {
-                bao::decode::decode(&in_map, &mut out_map, hash)?;
+                bao::decode::decode(&in_map, &mut out_map, &hash)?;
                 return Ok(());
             }
         }
     }
     // If one or both of the files weren't mappable, or if we're seeking, fall back to the reader.
-    let mut reader = bao::decode::Reader::new(in_file, hash);
+    let mut reader = bao::decode::Reader::new(in_file, &hash);
     if let Some(offset) = args.flag_seek {
         reader.seek(io::SeekFrom::Start(offset))?;
     }
+    io::copy(&mut reader, &mut out_file)?;
+    Ok(())
+}
+
+fn slice(args: &Args, in_file: File, mut out_file: File) -> Result<(), Error> {
+    // Slice extraction requires seek.
+    confirm_real_file(&in_file, "slicing input")?;
+    let mut reader = bao::decode::SliceExtractor::new(in_file, args.arg_start, args.arg_len);
+    io::copy(&mut reader, &mut out_file)?;
+    Ok(())
+}
+
+fn decode_slice(args: &Args, in_file: File, mut out_file: File) -> Result<(), Error> {
+    let hash = parse_hash(&args)?;
+    let mut reader = bao::decode::SliceReader::new(in_file, &hash, args.arg_start, args.arg_len);
     io::copy(&mut reader, &mut out_file)?;
     Ok(())
 }
@@ -200,4 +214,20 @@ fn maybe_memmap_output(
         assert_eq!(map.len() as u128, target_len);
         Some(map)
     })
+}
+
+fn confirm_real_file(file: &File, name: &str) -> Result<(), Error> {
+    if !file.metadata()?.is_file() {
+        Err(err_msg(format!("{} must be a real file", name)))
+    } else {
+        Ok(())
+    }
+}
+
+fn parse_hash(args: &Args) -> Result<[u8; bao::hash::HASH_SIZE], Error> {
+    let hash_vec = hex::decode(&args.arg_hash).map_err(|_| err_msg("invalid hex"))?;
+    if hash_vec.len() != bao::hash::HASH_SIZE {
+        return Err(err_msg("wrong length hash"));
+    };
+    Ok(*array_ref!(hash_vec, 0, bao::hash::HASH_SIZE))
 }
