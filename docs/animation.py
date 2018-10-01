54d4d8a8452db74cc2944a9fe37106e5dc20d18a
Jack O'Connor <oconnor663@gmail.com>
2018-10-01 13:51:33 -0400

    add some more color to animation.py and rerecord bao_hash.cast
    Note that to get the performance numbers in that recording, I do a
    `sudo systemctl disable gdm` to turn off the GUI entirely and then
    restart my laptop. With the desktop running, performance is about 10%
    worse (for everything, not just for bao). Capturing the result with
    asciinema also lets me replay it in a GUI terminal with a screen
    recorder, without the screen recording itself hurting hash
    performance.

--- docs/animation.py
+++ docs/animation.py
@@ -9,15 +9,17 @@ import termcolor
 TARGET = "/tmp/f"
 
 
-def char_by_char(s):
+def char_by_char(s, color=None):
     for c in s:
+        if color is not None:
+            c = termcolor.colored(c, color)
         time.sleep(0.03)
         print(c, end="")
         sys.stdout.flush()
 
 
 def comment(s):
-    char_by_char(termcolor.colored("# " + s + "\n", "yellow"))
+    char_by_char("# " + s + "\n", "yellow")
 
 
 def prompt():
@@ -25,21 +27,32 @@ def prompt():
     sys.stdout.flush()
 
 
-def run_command(command):
-    prompt()
-    char_by_char(command)
-    print()
+def shell_out(command):
     subprocess.run(["zsh", "-c", command])
+
+
+def time_hash(exe):
+    prompt()
+    char_by_char("time ")
+    char_by_char(exe, "red")
+    char_by_char(" " + TARGET)
+    print()
+    command = "time " + exe + " " + TARGET
+    shell_out(command)
 
 
 def main():
     comment("Create a gigabyte file.")
-    run_command("head -c 1000000000 /dev/zero > " + TARGET)
+    head_command = "head -c 1000000000 /dev/zero > " + TARGET
+    prompt()
+    char_by_char(head_command)
+    print()
+    shell_out(head_command)
     comment("Compare the time it takes for different programs to hash it.")
-    run_command("time md5sum " + TARGET)
-    run_command("time sha512sum " + TARGET)
-    run_command("time sha1sum " + TARGET)
-    run_command("time bao hash " + TARGET)
+    time_hash("sha512sum")
+    time_hash("md5sum")
+    time_hash("sha1sum")
+    time_hash("bao hash")
 
 
 if __name__ == "__main__":
